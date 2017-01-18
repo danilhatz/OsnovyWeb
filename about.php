659d150ee25508c9e5c0b4b8bb1ab52cff0bb4a7
danilhatz@gmail.com <cdJIl6592sa>
2017-01-19 01:09:44 +0200

    Initial commit

---
+++ about.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>eMeblef: about us</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/heroic-features.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse"
+                    data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.html">eMeblef</a>
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li>
+                    <a href="about.php">About</a>
+                </li>
+                <li>
+                    <a href="items.php">Goods</a>
+                </li>
+                <li>
+                    <a href="contacts.php">Contact</a>
+                </li>
+            </ul>
+            <ul class="nav navbar-nav pull-right">
+                <li>
+                    <a class="basket" href="basket.php">Basket</a>
+                </li>
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container -->
+</nav>
+<div class="container">
+    <header class="jumbotron hero-spacer">
+        <div class="about">
+            <h1 align="center">About us</h1>
+            <p>&nbsp;&nbsp;&nbsp;&nbsp; eMebel offers a unique selection of stylish, contemporary, and chic furniture online. Our online furniture range includes sofas, beds, dining tables, TV units, wardrobes, dressing tables, and lots more. All our wooden furniture designs are available online at emebel.com.ua On our portal, you can browse as many furniture designs across categories as you like, sort and filter, compare options, easily select and buy the ones you like, and stay updated about new and contemporary designs.
+                Furniture is what turns your house into a home. Well-designed furniture is not just functional or utilitarian (for example, tables and desks for work or cabinets and wardrobes for storage) but also a reflection of your taste and style. We have hundreds of furniture designs in solid wood, engineered wood, metal, etc., as well as soft furnishings and home decor items.
+                As an online furniture company, we know that service is as important as our products. That's why we pay attention to every detail of the process, from the time of ordering till your order reaches your home, and even afterwards.
+                The furniture you choose for a room can make or break it. The furniture designs you choose are what determine the personality of the room and say something about the inhabitants of the house. Modern furniture design is all about finding the right balance between design and functionality, while traditional designs have evolved over time, reflecting the fashion fads of each period. Whether you are on the lookout for modern designs or more traditional styles, there are some features to keep an eye out for.
+                Before you shop for home furniture designs, the first step is to picture the fully furnished rooms in your mind. What do you want each space to be used for? Will it be a private space, for family, or for entertaining? Secondly, consider the layout of each space, including the position of doors and windows, to understand how many and how big your furniture pieces can be, while still leaving enough room for people to walk around unobstructed.
+                If you have a large space, you have the luxury of adding a number of the latest furniture designs to the room, without overcrowding it, of course. For smaller rooms, it’s best that you look for multifunctional furniture designs that let you do more without taking up a lot of room. Once you identify what your room is going to be used for and what pieces you would like to furnish it with, you can proceed to find the right pieces online.
+            </p>
+            <a type="button" class="btn btn-about" href="contacts.php">Our contacts</a>
+            </td>
+        </div>
+    </header>
+<!-- Footer -->
+<footer>
+    <div class="collapse navbar-collapse" id="footer">
+        <ul class="nav navbar-nav">
+            <li>
+                <a href="about.php">About</a>
+            </li>
+            <li>
+                <a href="items.php">Goods</a>
+            </li>
+            <li>
+                <a href="contacts.php">Contact</a>
+            </li>
+            <li>
+                <a class="basket" href="basket.php">Basket</a>
+            </li>
+        </ul>
+    </div>
+    <div class="row">
+        <div class="col-lg-12">
+            <p align="center">Copyright &copy; eMeblef 2017</p>
+        </div>
+    </div>
+</footer>
+
+</div>
+
+<script src="js/jquery.js"></script>
+<!-- Bootstrap Core JavaScript -->
+<script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
+
