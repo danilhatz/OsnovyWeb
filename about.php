3de651bd5e1b55fedfb85d51484ad8654e2ba7ea
danilhatz <danilhatz@gmail.com>
2017-01-19 12:23:05 +0200

    new detail

--- about.php
+++ about.php
@@ -38,7 +38,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="index.html">eMeblef</a>
+            <a class="navbar-brand" href="index.php">eMeblef</a>
         </div>
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
--- items.php
+++ items.php
@@ -27,41 +27,8 @@
 </head>
 
 <body>
-<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-    <div class="container">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="index.html">eMeblef</a>
-        </div>
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav">
-                <li>
-                    <a href="about.php">About</a>
-                </li>
-                <li>
-                    <a href="items.php">Goods</a>
-                </li>
-                <li>
-                    <a href="contacts.php">Contact</a>
-                </li>
-            </ul>
-            <ul class="nav navbar-nav pull-right" >
-                <li>
-                    <a class="basket" href="basket.php">Basket</a>
-                </li>
-            </ul>
-        </div>
-        <!-- /.navbar-collapse -->
-    </div>
-    <!-- /.container -->
-</nav>
+
+<?php require_once "blocks/header.php" ?>
 
 <!-- Page Content -->
 <div class="container">
